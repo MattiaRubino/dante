530dcb9633795b210d724a98ba840d5822876748
MattiaRubino <132660543+MattiaRubino@users.noreply.github.com>
2026-08-28 21:01:52 +0200

    style(access): format auth route loader

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,9 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import {
-  AccessPage,
-  authSessionQueryOptions,
-} from '../features/access';
+import { AccessPage, authSessionQueryOptions } from '../features/access';
 
 export const Route = createFileRoute('/')({
   loader: ({ context }) =>
